40e1bfb645b9244f4e1ed45b79b394a0a3861b76
Hariharasudan S A <66832228+Hari3008@users.noreply.github.com>
2024-11-19 00:07:49 -0500

    view update

--- src/view/ImageView.java
+++ src/view/ImageView.java
@@ -1,9 +1,11 @@
 package view;
 
+import java.awt.image.BufferedImage;
+
 import controller.ImageController;
 
 public interface ImageView {
-  void refreshScreen(String imageName, String histogram);
+  void refreshScreen(BufferedImage imageName, BufferedImage histogram);
 
   void splitView(String imageName);
 
